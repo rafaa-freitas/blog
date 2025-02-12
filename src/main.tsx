f519b6c5a8c37fa9145be625b879237cdea15d6c
Rafael Freitas <rafael.freitasfa@gmail.com>
2025-02-12 19:44:39 -0300

    fix: Removing unnecessary react import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import React from 'react';
 
 createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
